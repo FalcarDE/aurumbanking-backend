0fbb7d2253716a2d737a1a2ae100bf7b4c4b4912
FalcarDE <hoangtrananh1234@gamil.com>
2024-05-29 01:42:18 +0200

    init new project structure

---
+++ src/native-test/java/de/fhe/cc/team4/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package de.fhe.cc.team4;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
