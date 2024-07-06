dbafc623a440e5fba0c8f38babf30d468cd51b44
FalcarDE <hoangtrananh1234@gamil.com>
2024-07-06 02:37:02 +0200

    adjust run.sh

--- run.sh
+++ run.sh
@@ -1,35 +1,79 @@
 #!/bin/bash
 
-echo "Building service containers..."
-#--------------------------------------------
-echo "Building customer-information-service..."
-cd customer-information-service || exit
-./gradlew build -x test -Dquarkus.container-image.build=true
-cd ..
-echo "Building customer-information-service... done."
-#--------------------------------------------
-echo "Building depot-service..."
-cd depot-service || exit
-./gradlew build -x test -Dquarkus.container-image.build=true
-cd ..
-echo "Building depot-service... done."
-#--------------------------------------------
-echo "Building support-service..."
-cd support-service || exit
-./gradlew build -x test -Dquarkus.container-image.build=true
-cd ..
-echo "Building support-service... done."
-#--------------------------------------------
-echo "Building login-service..."
-cd login-service || exit
-./gradlew build -x test -Dquarkus.container-image.build=true
-cd ..
-echo "Building login-service... done."
-#--------------------------------------------
-echo "Building transaction-service..."
-cd transaction-service || exit
-./gradlew build -x test -Dquarkus.container-image.build=true
-cd ..
-echo "Building transaction-service... done."
-#--------------------------------------------
-echo "Building service containers... done."
+# Funktion zum Bauen eines Services
+build_service() {
+    service_name=$1
+    echo "Building $service_name..."
+    cd $service_name || exit
+    ./gradlew build -x test -Dquarkus.container-image.build=true
+    cd ..
+    echo "Building $service_name... done."
+}
+
+# Funktion zum Starten eines Docker Compose Setups
+start_docker_compose() {
+    compose_file=$1
+    echo "Starting Docker Compose setup: $compose_file..."
+    docker-compose -f $compose_file up -d
+    echo "Starting Docker Compose setup: $compose_file... done."
+}
+
+# Frage, ob die Services gebaut werden sollen
+read -p "Do you want to build the service containers? (yes/y or no/n): " build_services_choice
+if [[ $build_services_choice == "yes" || $build_services_choice == "y" ]]; then
+    echo "Building service containers..."
+    build_service "customer-information-service"
+    build_service "depot-service"
+    build_service "support-service"
+    build_service "login-service"
+    build_service "transaction-service"
+    echo "Building service containers... done."
+else
+    echo "Skipping the build of service containers."
+fi
+
+# Optionen für Docker Compose Setups
+options=(
+    "docker-compose-prod.yml"
+    "monitoring/docker-compose-metrics.yml"
+    "monitoring/docker-compose-metrics-ui.yml"
+    "monitoring/docker-compose-tracing.yml"
+)
+
+echo "Which Docker Compose setups would you like to start?"
+echo "Enter the numbers corresponding to your choices, separated by spaces (e.g., 1 3):"
+for i in "${!options[@]}"; do
+    echo "$((i+1)). ${options[$i]}"
+done
+echo "$(( ${#options[@]} + 1 )). All Docker Compose setups"
+
+# Benutzerauswahl einlesen
+read -p "Your choices: " choices
+
+# Auswahl ausführen
+for choice in $choices; do
+    case $choice in
+        1)
+            start_docker_compose "docker-compose-prod.yml"
+            ;;
+        2)
+            start_docker_compose "monitoring/docker-compose-metrics.yml"
+            ;;
+        3)
+            start_docker_compose "monitoring/docker-compose-metrics-ui.yml"
+            ;;
+        4)
+            start_docker_compose "monitoring/docker-compose-tracing.yml"
+            ;;
+        5)
+            for i in "${!options[@]}"; do
+                start_docker_compose "${options[$i]}"
+            done
+            ;;
+        *)
+            echo "Invalid choice: $choice"
+            ;;
+    esac
+done
+
+echo "All selected services started successfully."
