dbd90cb966648558a97877b45a7ed33b9b592fbe
FalcarDE <hoangtrananh1234@gamil.com>
2024-07-22 19:25:42 +0200

    add ressource-test

--- customer-information-service/src/main/kotlin/de/fhe/cc/team4/aurumbanking/domain/DomainModelClasses.kt
+++ customer-information-service/src/main/kotlin/de/fhe/cc/team4/aurumbanking/domain/DomainModelClasses.kt
@@ -1,35 +1,39 @@
 package de.fhe.cc.team4.aurumbanking.domain
+
 import java.time.LocalDateTime
 import java.util.*
 
 
 data class CustomerInformationDomainModel(
-    var id:          Long = 0,
-    var firstname:   String,
-    var lastname:    String,
-    var birthDate:   Date,
-    var created:     LocalDateTime,
-    var lastestLogin:LocalDateTime,
-    var streetName:  String,
+    var id: Long = 0,
+    var firstname: String,
+    var lastname: String,
+    var birthDate: Date,
+    var created: LocalDateTime,
+    var lastestLogin: LocalDateTime,
+    var streetName: String,
     var housenumber: String,
-    var city:        String,
-    var country:     String,
-    var username:    String,
-    var email:       String,
+    var city: String,
+    var country: String,
+    var zipcode: String,
+    var username: String,
+    var email: String,
     var phoneNumber: String,
-    var password:    String,
+    var password: String,
     var profileImage: ByteArray? = null
-){
-    constructor():
-            this(firstname = "testFirstName",
+) {
+    constructor() :
+            this(
+                firstname = "testFirstName",
                 lastname = "testLastname",
-                birthDate= Date(1990, 1,1),
-                created=LocalDateTime.now(),
-                lastestLogin=LocalDateTime.now(),
-                streetName="testStreetName",
-                housenumber="1",
-                city="testCity",
-                country="testCountry",
+                birthDate = Date(1990, 1, 1),
+                created = LocalDateTime.now(),
+                lastestLogin = LocalDateTime.now(),
+                streetName = "testStreetName",
+                housenumber = "1",
+                city = "testCity",
+                country = "testCountry",
+                zipcode = "testZipcode",
                 username = "testUsername",
                 email = "testEmail",
                 phoneNumber = "testPhoneNumber",
