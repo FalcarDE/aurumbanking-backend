8d3c789b6f7beef32e2c3924b336526b58c2a906
mi3512ne <milena.neumann@fh-erfurt.de>
2024-08-12 17:47:10 +0200

    test for createSupportEntry

--- support-service/src/test/java/de/fhe/cc/team4/aurumbanking/repositories/SupportRepositoryTest.kt
+++ support-service/src/test/java/de/fhe/cc/team4/aurumbanking/repositories/SupportRepositoryTest.kt
@@ -1,9 +1,95 @@
-package de.fhe.cc.team4.aurumbanking.repositories;
+package de.fhe.cc.team4.aurumbanking.repositories
 
-import io.quarkus.test.junit.QuarkusTest;
+import de.fhe.cc.team4.aurumbanking.model.SupportRepositoryImp
+import de.fhe.cc.team4.aurumbanking.util.createSupportEntry
+import de.fhe.cc.team4.aurumbanking.util.createSupportEntry1
+import de.fhe.cc.team4.aurumbanking.util.createSupportEntry2
+import de.fhe.cc.team4.aurumbanking.util.createAllSupportEntries
+
+import io.quarkus.test.TestReactiveTransaction
+import io.quarkus.test.junit.QuarkusTest
+import io.quarkus.test.vertx.UniAsserter
+import jakarta.inject.Inject
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 @QuarkusTest
-public class SupportRepositoryTest {
+class SupportRepositoryTest {
+
+    @Inject
+    lateinit var supportRepository: SupportRepositoryImp
+
+    @Test
+    @TestReactiveTransaction
+    fun testCreateSupportRequest(asserter: UniAsserter) {
+        val supportRequest = createSupportEntry()
+
+        asserter.assertThat(
+            {
+                this.supportRepository.persist(supportRequest) //  this.supportRepository.persistNewSupportInformation(supportRequest)
+                    .replaceWith(this.supportRepository.findSupportRequestById(supportRequest.id))
+            },
+            { foundRequest ->
+                assertNotNull(foundRequest)
+
+                foundRequest?.let {
+                    assertEquals(12345, it.customerId)
+                    assertEquals("Konto", it.type)
+                    assertEquals( "Wie kann ich mein Konto erstellen?", it.message)
+                }
+            }
+        )
+    }
+
+/*
+    @Test
+    @TestReactiveTransaction
+    fun testDeleteSupportRequestsByCustomerId(asserter: UniAsserter) {
+        // Erstelle mehrere Support-Einträge mit der gleichen customerId
+        val supportRequest1 = createSupportEntry1()
+        val supportRequest2 = createSupportEntry2()
+        asserter.assertThat(
+            {
+                // Persistiere beide Support-Anfragen
+                this.supportRepository.persist(supportRequest1) // this.supportRepository.persistNewSupportInformation(supportRequest1)
+                    .replaceWith(this.supportRepository.persist(supportRequest2))
+                    // Lösche alle Support-Anfragen für diese customerId
+                    .replaceWith(this.supportRepository.deleteAllSupportRequestByCustomerId(12345))
+                    // Überprüfe, ob alle Anfragen gelöscht wurden, indem du versuchst, sie zu finden
+                    .replaceWith(this.supportRepository.findSupportRequestByCustomerId(12345))
+            },
+            { foundRequests ->
+                // Überprüfe, dass keine Anfragen mehr gefunden werden
+                assertTrue(foundRequests.isEmpty())
+            }
+        )
+    }
 
 
+    @Test
+    @TestReactiveTransaction
+    fun testFindSupportRequestsByType(asserter: UniAsserter) {
+        // Hole die Liste der Testdaten
+        val supportEntries = createAllSupportEntries()
+
+        // Wähle die ersten beiden Support-Einträge aus der Liste aus
+        val supportRequest1 = supportEntries[0].apply { type = "Technical Support" }
+        val supportRequest2 = supportEntries[1].apply { type = "Customer Service" }
+
+        asserter.assertThat(
+            {
+                this.supportRepository.persist(supportRequest1)
+                    .replaceWith(this.supportRepository.persist(supportRequest2))
+                    .replaceWith(this.supportRepository.getAllRequestsByType("Technical Support"))
+            },
+            { supportRequests ->
+                assertNotNull(supportRequests)
+                assertEquals(1, supportRequests.size)
+                assertEquals("Technical Support", supportRequests[0].type)
+            }
+        )
+    }
+
+
+ */
 }
---
+++ support-service/src/test/java/de/fhe/cc/team4/aurumbanking/util/TestData.kt
@@ -0,0 +1,75 @@
+package de.fhe.cc.team4.aurumbanking.util
+
+import de.fhe.cc.team4.aurumbanking.model.SupportEntityModel
+import java.time.LocalDateTime
+
+fun createSupportEntry(): SupportEntityModel {
+    val newSupportEntry = SupportEntityModel()
+    newSupportEntry.customerId = 12345
+    newSupportEntry.dateTime = LocalDateTime.now()
+    newSupportEntry.type = "Konto"
+    newSupportEntry.message = "Wie kann ich mein Konto erstellen?"
+
+    return newSupportEntry
+}
+
+fun createSupportEntry1(): SupportEntityModel {
+    val newSupportEntry = SupportEntityModel()
+    newSupportEntry.customerId = 12345
+    newSupportEntry.dateTime = LocalDateTime.now()
+    newSupportEntry.type = "Konto"
+    newSupportEntry.message = "Wie kann ich mein Konto schließen?"
+
+    return newSupportEntry
+}
+
+fun createSupportEntry2(): SupportEntityModel {
+    val newSupportEntry = SupportEntityModel()
+    newSupportEntry.customerId = 67890
+    newSupportEntry.dateTime = LocalDateTime.now()
+    newSupportEntry.type = "Technischer Support"
+    newSupportEntry.message = "Ich kann mich nicht in mein Konto einloggen."
+
+    return newSupportEntry
+}
+
+fun createSupportEntry3(): SupportEntityModel {
+    val newSupportEntry = SupportEntityModel()
+    newSupportEntry.customerId = 12345
+    newSupportEntry.dateTime = LocalDateTime.now()
+    newSupportEntry.type = "Zahlungen"
+    newSupportEntry.message = "Wie kann ich eine Überweisung durchführen?"
+
+    return newSupportEntry
+}
+
+fun createSupportEntry4(): SupportEntityModel {
+    val newSupportEntry = SupportEntityModel()
+    newSupportEntry.customerId = 67890
+    newSupportEntry.dateTime = LocalDateTime.now()
+    newSupportEntry.type = "Konto"
+    newSupportEntry.message = "Welche Dokumente benötige ich, um ein Konto zu eröffnen?"
+
+    return newSupportEntry
+}
+
+fun createSupportEntry5(): SupportEntityModel {
+    val newSupportEntry = SupportEntityModel()
+    newSupportEntry.customerId = 13579
+    newSupportEntry.dateTime = LocalDateTime.now()
+    newSupportEntry.type = "Kundendienst"
+    newSupportEntry.message = "Wie kann ich meine persönlichen Daten aktualisieren?"
+
+    return newSupportEntry
+}
+
+fun createAllSupportEntries(): List<SupportEntityModel> {
+    val supportEntry0 = createSupportEntry()
+    val supportEntry1 = createSupportEntry1()
+    val supportEntry2 = createSupportEntry2()
+    val supportEntry3 = createSupportEntry3()
+    val supportEntry4 = createSupportEntry4()
+    val supportEntry5 = createSupportEntry5()
+
+    return listOf(supportEntry0, supportEntry1, supportEntry2, supportEntry3, supportEntry4, supportEntry5)
+}
