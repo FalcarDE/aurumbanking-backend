e965e471ac59724ec1317ccd2a45065e783005c3
mi3512ne <milena.neumann@fh-erfurt.de>
2024-08-16 12:00:29 +0200

    erstellte resource tests : insertSupportRequests, getRequestById, getAllByType, createAndDeleteById, delete by customerId

--- support-service/src/test/java/de/fhe/cc/team4/aurumbanking/ressource/SupportResourceTest.kt
+++ support-service/src/test/java/de/fhe/cc/team4/aurumbanking/ressource/SupportResourceTest.kt
@@ -1,9 +1,238 @@
-package de.fhe.cc.team4.aurumbanking.ressource;
+package de.fhe.cc.team4.aurumbanking.resources
 
-import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.common.http.TestHTTPEndpoint
+import io.quarkus.test.junit.QuarkusTest
+import io.restassured.http.ContentType
+import io.restassured.module.kotlin.extensions.Extract
+import io.restassured.module.kotlin.extensions.Given
+import io.restassured.module.kotlin.extensions.Then
+import io.restassured.module.kotlin.extensions.When
+import org.junit.jupiter.api.Test
+import org.hamcrest.CoreMatchers.equalTo
 
 @QuarkusTest
+@TestHTTPEndpoint(SupportResource::class)
 class SupportResourceTest {
 
+    /**
+     * Tests for inserting two support requests
+     */
+    @Test
+    fun `insert new support request`() {
+        val newSupportRequest = mapOf(
+       
+            "message" to "I have an issue with my account.",
+            "type" to "technical",
+            "customerId" to 1L
+        )
 
+        // Insert new support request
+        Given {
+            contentType(ContentType.JSON)
+            accept(ContentType.JSON)
+            body(newSupportRequest)
+        } When {
+            post()
+        } Then {
+            statusCode(201)
+        } Extract {
+            response()
+        }
+    }
+
+    @Test
+    fun `insert second support request`() {
+        val newSupportRequest = mapOf(
+            "message" to "I forgot what to ask.",
+            "type" to "technical",
+            "customerId" to 2L
+        )
+
+        // Insert new support request
+        Given {
+            contentType(ContentType.JSON)
+            accept(ContentType.JSON)
+            body(newSupportRequest)
+        } When {
+            post()
+        } Then {
+            statusCode(201)
+        } Extract {
+            response()
+        }
+    }
+
+    @Test
+    fun `insert third support request`() {
+        val newSupportRequest = mapOf(
+            "message" to "blablub.",
+            "type" to "transaction",
+            "customerId" to 3L
+        )
+
+        // Insert new support request
+        Given {
+            contentType(ContentType.JSON)
+            accept(ContentType.JSON)
+            body(newSupportRequest)
+        } When {
+            post()
+        } Then {
+            statusCode(201)
+        } Extract {
+            response()
+        }
+    }
+
+    /**
+     * Test for getting support request by ID
+     */
+    @Test
+    fun `get support request by id`() {
+        val requestId = 1L
+
+        // Get support request by ID
+        Given {
+            accept(ContentType.JSON)
+        } When {
+            get("/$requestId")
+        } Then {
+            statusCode(200)
+            body("id", equalTo(requestId.toInt()))
+        }
+    }
+
+
+    /**
+     * Test for getting all support requests by type
+     */
+    @Test
+    fun `get all support requests by type`() {
+        val requestType = "technical"
+
+        // Get all support requests by type
+        val response = Given {
+            accept(ContentType.JSON)
+        } When {
+            get("/get-all-support-reqeuests-by-type/$requestType")
+        } Then {
+            statusCode(200)  // Check for success response
+        } Extract {
+            response()  // Extract the full response object
+        }
+
+        // Extract and log specific details from the response
+        val jsonResponse = response.jsonPath()
+        val supportRequests = jsonResponse.getList<Map<String, Any>>("") // Extract the list of support requests
+
+        // Assertions based on the actual response
+        assert(supportRequests.isNotEmpty()) { "No support requests found" }
+        assert(supportRequests.size == 2) { "Expected 2 support request, but got ${supportRequests.size}" }
+    }
+
+
+    /**
+     * Test for getting all support requests by customer ID
+     */
+    @Test
+    fun `get all support requests by customer id`() {
+        val customerId = 2L
+
+        // Get all support requests by customer ID
+        val response = Given {
+            accept(ContentType.JSON)
+        } When {
+            get("/get-all-support-reqeuests-by-customer-id/$customerId")
+        } Then {
+            statusCode(200)
+        } Extract {
+            response()  // Extract the full response object
+        }
+
+        // Extract and log specific details from the response
+        val jsonResponse = response.jsonPath()
+        val supportRequests = jsonResponse.getList<Map<String, Any>>("") // Extract the list of support requests
+
+        // Assertions based on the actual response
+        assert(supportRequests.isNotEmpty()) { "No support requests found for customer with ID $customerId" }
+        assert(supportRequests.size == 1) { "Expected 1 support request, but got ${supportRequests.size}" }
+    }
+
+
+    /**
+     * Test for creating and deleting a support request by customer ID
+     */
+    @Test
+    fun `create and delete a support request by id`() {
+        val newSupportRequest = mapOf(
+            "message" to "I have an issue with my account.",
+            "type" to "technical",
+            "customerId" to 1L
+        )
+
+        // 1. Create new support request
+        val response = Given {
+            contentType(ContentType.JSON)
+            accept(ContentType.JSON)
+            body(newSupportRequest)
+        } When {
+            post()
+        } Then {
+            statusCode(201) // Expecting HTTP 201 Created
+        } Extract {
+            response()
+        }
+
+        // Extract ID from location-header
+        val locationHeader = response.getHeader("Location")
+        val requestId = locationHeader?.split("/")?.last()?.toLong()
+            ?: throw IllegalStateException("Location header is missing or does not contain an ID")
+
+        // 2. Delete the support request
+        Given {
+            accept(ContentType.ANY)
+        } When {
+            delete("/deleteRequestById/$requestId")
+        } Then {
+            statusCode(200) // Expecting HTTP 200 OK
+        }
+
+        // 3. Check if the support request is deleted
+        Given {
+            accept(ContentType.ANY)
+        } When {
+            get("/$requestId")
+        } Then {
+            statusCode(404) // Expecting HTTP 404 Not Found after deletion
+        }
+    }
+
+
+
+
+    @Test
+    fun `delete request by customer id`() {
+        val customerId = 1L
+
+        Given {
+            accept(ContentType.JSON)
+        } When {
+            delete("/deleteRequestByCustomerId/$customerId")
+        } Then {
+            statusCode(200)
+        }
+
+        // Verify the support request is no longer available
+        Given {
+            accept(ContentType.JSON)
+        } When {
+            get("/get-all-support-reqeuests-by-customer-id/$customerId")
+        } Then {
+            statusCode(200)  // Expecting 404 Not Found after deletion
+        } Extract {
+            response()
+        }
+    }
 }
+
+
